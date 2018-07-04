e80cb3a90a7a2c41a21f465cd885887919c02f6d
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-07-04 16:31:35 -0600

    adds partial for comments

--- spec/features/user_can_see_article_on_show_page_spec.rb
+++ spec/features/user_can_see_article_on_show_page_spec.rb
@@ -1,9 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe 'user sees one article' do
-  describe 'they link from the articles index' do
-    it 'displays infromation for one article' do
-      article = Article.create!(title: "Title 1", body: "Body 2")
+describe "user sees one article" do
+  describe "they link from the article index" do
+    it "displays information for one article" do
+      article = Article.create!(title: "New Title", body: "New Body")
       comment_1 = article.comments.create(author_name: "Me", body: "Commenty comments")
       comment_2 = article.comments.create(author_name: "You", body: "So much to say")
 
@@ -15,7 +15,7 @@ describe 'user sees one article' do
       expect(page).to have_content(article.body)
       expect(page).to have_content(comment_1.author_name)
       expect(page).to have_content(comment_1.body)
-      expet(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.author_name)
       expect(page).to have_content(comment_2.body)
     end
   end
