5007f7f8ba0d3c25241cfab178e5ccacc763f350
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-07-04 17:46:26 -0600

    adds tag and tagging model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Article, type: :model do
   describe "relationships" do
     it {should have_many(:comments)}
+    it {should have_many(:taggings)}
   end
   describe "validations" do
     it {should validate_presence_of(:title)}
