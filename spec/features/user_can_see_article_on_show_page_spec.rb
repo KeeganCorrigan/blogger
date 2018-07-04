35e2e2a9e3daff19943e810d907055e45ab6c357
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-07-04 10:21:48 -0600

    Adds view for show page

--- spec/features/user_can_see_article_on_show_page_spec.rb
+++ spec/features/user_can_see_article_on_show_page_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 describe 'user sees one article' do
   describe 'they link from the articles index' do
     it 'displays infromation for one article' do
-      article_1 = Article.create!(title: "Title 1", body: "Body 2")
+      article = Article.create!(title: "Title 1", body: "Body 2")
 
-      visist '/articles'
+      visit '/articles'
 
       click_link article.title
 
-      expect_page.to have_content(article.title)
-      expect_page.to have_content(article.body)
+      expect(page).to have_content(article.title)
+      expect(page).to have_content(article.body)
     end
   end
 end
