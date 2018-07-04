b330457a59cda8035607a4bc39c76870c1c7f3b7
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-07-04 15:48:32 -0600

    adds flash functionality to create and delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,11 +18,15 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article Created!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article.destroy
+
+    flash.notice = "Article Deleted!"
 
     redirect_to articles_path
   end
