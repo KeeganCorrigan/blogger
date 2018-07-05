514af05ac5237176205060fc88c843b1f727799b
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-07-04 19:34:04 -0600

    adds and tests delete function for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,22 @@
 class TagsController < ApplicationController
+  before_action :set_tag, only: [:show, :destroy, :edit, :update]
 
   def index
     @tags = Tag.all
   end
 
   def show
-    @tag = Tag.find(params[:id])
   end
 
+  def destroy
+    @tag.destroy
+
+    redirect_to tags_path
+  end
+
+  private
+
+  def set_tag
+    @tag = Tag.find(params[:id])
+  end
 end
